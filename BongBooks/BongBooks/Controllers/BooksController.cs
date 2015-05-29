02668618a2f589de05fd62ca47d8f2a527a2f1dc
Sodabi <guyoungchung@aol.com>
2015-05-29 17:01:32 -0400

    BookList

---
+++ BongBooks/BongBooks/Controllers/BooksController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BongBooks.Controllers
+{
+    public class BooksController : Controller
+    {
+        //
+        // GET: /Books/
+
+        public ActionResult SellList()
+        {
+            return View();
+        }
+
+    }
+}
